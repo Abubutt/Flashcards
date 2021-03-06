db1bce7e71ef5eba7956aaf42e229b8622ed0151
Abu Butt <abubutt578@gmail.com>
2021-03-06 15:06:53 -0500

    lab 2

---
+++ Flashcards/ViewControllers/CreationViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Abu Butt on 3/6/21.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardsController: ViewController!
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        
+        let questionText = questionTextField.text
+        let answerText = answerTextField.text
+        
+        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
+        
+        dismiss(animated: true)
+    }
+    
+
+}
--- Flashcards/ViewControllers/ViewController.swift
+++ Flashcards/ViewControllers/ViewController.swift
@@ -21,5 +21,17 @@ class ViewController: UIViewController {
         frontLabel.isHidden = true
     }
     
+    func updateFlashcard(question: String, answer: String) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardsController = self
+    }
+    
 }
 
